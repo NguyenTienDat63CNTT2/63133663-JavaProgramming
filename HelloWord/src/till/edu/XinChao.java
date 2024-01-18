dd74d205587303d07f0426ec1d230191964e5ebe
NguyenTienDat63CNTT2 <115135714+NguyenTienDat63CNTT2@users.noreply.github.com>
2024-01-18 20:04:31 +0700

    Cập nhật code
    Sửa mới và thêm dòng

--- HelloWord/src/till/edu/XinChao.java
+++ HelloWord/src/till/edu/XinChao.java
@@ -3,5 +3,6 @@ package till.edu;
 public class XinChao {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("Nguyen Tien Dat - 63.CNTT-2");
 	}
 }
